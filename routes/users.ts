e1c7279b85e439e0c2c4376084803e18b114d092
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-06 17:16:20 +0300

    deleteUser added

--- routes/users.ts
+++ routes/users.ts
@@ -18,8 +18,27 @@ async function DBRequest(request: string): Promise<string | void> {
     })
 }
 
+function SendError(res: any, ErrorMessage: string): void {
+
+}
+
 router.get('/', async (req, res, next) => {
     res.send(await DBRequest("SELECT * FROM `users`"));
+});
+
+router.get('/deleteUser', async (req, res, next) => {
+    if (!req.query.userid) {
+        SendError(res, "Введите корректный userid")
+        return;
+    }
+
+    // const users = await DBRequest(`SELECT * FROM \`users\` WHERE \`users\`.\`userid\` = ${parseInt(req.query.userid as string)})
+    const users = await DBRequest(`SELECT * FROM \`users\` WHERE \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`) as string
+    if (users.length === 0) {
+        SendError(res, "Такого пользователя не существует.")
+        return;
+    }
+    await DBRequest(`DELETE FROM \`users\` WHERE \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`);
 });
 
 module.exports = router;
