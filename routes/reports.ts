9488e1d7f003cacb643efc0813be3878fa7bae64
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-06 20:10:30 +0300

    AutoTruncate Table

--- routes/reports.ts
+++ routes/reports.ts
@@ -67,7 +67,7 @@ router.post('/', async (req, res, next) => {
     const cluster = JSON.parse(response as string)
     for (let x = 0; x < cluster.length; x++) {
         console.log(x)
-        await DBRequest(`INSERT INTO \`reports\` (\`userid\`, \`body\`) VALUES (${users[0].userid}, '${JSON.stringify(cluster[0])}')`)
+        await DBRequest(`INSERT INTO \`reports\` (\`userid\`, \`body\`) VALUES (${users[0].userid}, '${JSON.stringify(cluster[x])}')`)
     }
     res.send({
         notification: "Отчет добавлен в базу данных",
