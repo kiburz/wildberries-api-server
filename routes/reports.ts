b03980d8f76df6a7e816fb0e31f8e355cccdc161
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-06 19:35:57 +0300

    JSON parse fix

--- routes/reports.ts
+++ routes/reports.ts
@@ -63,7 +63,7 @@ router.post('/', async (req, res, next) => {
         return;
     }
 
-    await DBRequest(`INSERT INTO \`reports\` (\`body\`, \`userid\`) VALUES ('{${JSON.stringify(response)}}', ${users[0].userid})`).then(() => {
+    await DBRequest(`INSERT INTO \`reports\` (\`body\`, \`userid\`) VALUES ('${JSON.parse(JSON.stringify(response))}', ${users[0].userid})`).then(() => {
         res.send({
             notification: "Отчет добавлен в базу данных",
             userid: users.userid,
