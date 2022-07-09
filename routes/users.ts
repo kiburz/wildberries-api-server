0029719dfabd61ff717e7344301623adc7160c51
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-09 17:08:58 +0300

    Autodeletion reports added and users editioning

--- routes/users.ts
+++ routes/users.ts
@@ -45,6 +45,27 @@ router.post('/', async (req, res, next) => {
     });
 });
 
+router.put('/', async (req, res, next) => {
+    if (req.query.userid && (req.query.name || req.query.surname || req.query.api_key)) {
+        console.log(req.query)
+        const user = await DBRequest(`SELECT * FROM \`users\` WHERE \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`) as string
+        if (user.length === 0) {
+            SendError(res, "Такого пользователя не существует.")
+            return;
+        }
+        if (req.query.name)
+            await DBRequest(`UPDATE \`users\` SET \`name\` = ${req.query.name} WHERE  \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`)
+        if (req.query.surname)
+            await DBRequest(`UPDATE \`users\` SET \`surname\` = ${req.query.surname} WHERE  \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`)
+        if (req.query.api_key)
+            await DBRequest(`UPDATE \`users\` SET \`api_key\` = ${req.query.api_key} WHERE  \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`)
+
+    } else {
+        SendError(res, "Введите корректные данные для изменения пользователя")
+        return;
+    }
+});
+
 router.delete('/', async (req, res, next) => {
     if (!req.query.userid) {
         SendError(res, "Введите корректный userid")
@@ -56,6 +77,7 @@ router.delete('/', async (req, res, next) => {
         SendError(res, "Такого пользователя не существует.")
         return;
     }
+    await DBRequest(`DELETE FROM \`reports\` WHERE \`reports\`.\`userid\` = ${parseInt(req.query.userid as string)}`)
     await DBRequest(`DELETE FROM \`users\` WHERE \`users\`.\`userid\` = ${parseInt(req.query.userid as string)}`).then(() => {
         SendNotification(res, "Пользователь удален")
     });
