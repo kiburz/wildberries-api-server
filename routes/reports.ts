266a156686e0a4fd80eec5858414c2a7ae0e7c61
N3wSk1Y <miner.fedenko@gmail.com>
2022-08-01 22:05:39 +0300

    Disconnection handling

--- routes/reports.ts
+++ routes/reports.ts
@@ -19,6 +19,26 @@ async function DBRequest(request: string): Promise<any | void> {
     })
 }
 
+function handleDisconnect() {
+    connection.connect(function(err) {
+        if(err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
 async function HTTPRequest(options: any): Promise<any | void> {
     return new Promise((resolve, reject) => {
         request(options, function (error: any, response: any) {
